f95688dfeb3fed3c609b72c99c2d801a899edf58
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2021-01-17 00:36:56 -0300

    api

--- src/container/main/index.jsx
+++ src/container/main/index.jsx
@@ -1,7 +1,19 @@
-import React, {memo} from 'react'
+import React, {memo, useCallback, useState, useEffect } from 'react'
+import Api from '../../api'
 
 
 function Main() {
+    const [ data, setData ] = useState({})
+    const [country, setContry] = useState('brazil')
+    
+    const getCovidData = useCallback((country) => {
+        Api.getCountry(country)
+        .then(data => setData(data))
+    }, [])
+
+    useEffect(() => {
+        getCovidData(country)
+    }, [getCovidData, country])
     return (
         <div>
             teste
